cedc2cbed2e93f3412dac50908d7b57e6ee1709c
Arber Gjonaj <agjonaj@ikub.local>
2021-01-12 14:38:18 +0100

    ngOnInit lifecycle hook

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Passenger } from './../../Models/passenger.interface';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -30,42 +30,49 @@ import { Component } from '@angular/core';
 
   `
 })
-export class PassengerDashboardComponent{
+export class PassengerDashboardComponent implements OnInit{
   Passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: "Stephen",
-      checkedIn: true,
-      checkInDate: 15135125454235,
-      children:[{name:"bob",age:3},{name:"PopMonster",age:12}]
-    },
-    {
-      id: 2,
-      fullname: "Rose",
-      checkedIn: false,
-      checkInDate: null,
-      children:[{name:"bob",age:3}]
-    },
-    {
-      id: 3,
-      fullname: "Arber",
-      checkedIn: true,
-      checkInDate: 15135125454235,
-      children:null
-    },
-    {
-      id: 4,
-      fullname: "Renald",
-      checkedIn: false,
-      checkInDate: null,
-      children:null
-    },
-    {
-      id: 5,
-      fullname: "Fabjola",
-      checkedIn: true,
-      checkInDate: 15135125454235,
-      children:null
-    },
   ];
+  constructor(){
+
+  }
+  ngOnInit(){
+    console.log("ngOnInit")
+    this.Passengers=[
+      {
+        id: 1,
+        fullname: "Stephen",
+        checkedIn: true,
+        checkInDate: 15135125454235,
+        children:[{name:"bob",age:3},{name:"PopMonster",age:12}]
+      },
+      {
+        id: 2,
+        fullname: "Rose",
+        checkedIn: false,
+        checkInDate: null,
+        children:[{name:"bob",age:3}]
+      },
+      {
+        id: 3,
+        fullname: "Arber",
+        checkedIn: true,
+        checkInDate: 15135125454235,
+        children:null
+      },
+      {
+        id: 4,
+        fullname: "Renald",
+        checkedIn: false,
+        checkInDate: null,
+        children:null
+      },
+      {
+        id: 5,
+        fullname: "Fabjola",
+        checkedIn: true,
+        checkInDate: 15135125454235,
+        children:null
+      },]
+  }
 }
