c32c85e206557050ae2f9c337e8ed9537939fbc4
Arber Gjonaj <agjonaj@ikub.local>
2021-01-12 08:56:18 +0100

    Interpolation and expressions

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,14 +4,22 @@ import { Component } from "@angular/core";
   styleUrls:['app.component.scss'],
   template:`
   <div class="app" >
-   {{title}}
-   
+   {{title + "!"}}
+   <div>
+   {{number1+number2}}
+   </div>
+   <div>
+   {{isHappy?':D':':('}}
+   </div>
    </div>
   `
 })
 export class AppComponent {
   groceries: string[];
   title: string;
+  isHappy:boolean=false;
+  number1:number = 1;
+  number2:number = 2;
   constructor() {
     this.title = "Ultimate Angular";
   }
