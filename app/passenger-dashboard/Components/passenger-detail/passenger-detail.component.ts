2d5b10aabca5d836dd0d768642999bfae4ac4c29
Arber Gjonaj <agjonaj@ikub.local>
2021-01-12 16:05:04 +0100

    Dynamic @Input values with *ngFor

--- app/passenger-dashboard/Components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/Components/passenger-detail/passenger-detail.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Passenger } from "./../../Models/passenger.interface";
+import { Component,Input } from "@angular/core";
 
 @Component({
- selector:'passenger-detail',
- template:`
- <div>
- Detail Component
- </div>
- `
+  selector: "passenger-detail",
+  styleUrls:['passenger-detail.component.scss'],
+  template: `
+    <div>
+      <span class="status" [class.checked-in]="detail.checkedIn"></span>
+      {{ detail.fullname }}
+      <div class="date">
+        Check in date:
+        {{
+          detail.checkInDate
+            ? (detail.checkInDate | date: "yMMMd" | uppercase)
+            : "Not checked In"
+        }}
+      </div>
+    </div>
+  `,
 })
-export class PassengerDetailComponent{
-  constructor(){}
+export class PassengerDetailComponent {
+  @Input()
+  detail: Passenger;
+  constructor() {}
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,84 +1,73 @@
-import { Passenger } from './../../Models/passenger.interface';
-import { Component, OnInit } from '@angular/core';
-
+import { Passenger } from "./../../Models/passenger.interface";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'passenger-dashboard',
-  styleUrls:["passenger-dashboard.component.scss"],
+  selector: "passenger-dashboard",
+  styleUrls: ["passenger-dashboard.component.scss"],
   template: `
-  <div>
-    <passenger-count
-    [items]="passengers">
-    </passenger-count>
+    <div>
+      <passenger-count [items]="passengers"> </passenger-count>
 
-    <passenger-detail>
-    </passenger-detail>
-  
+      <passenger-detail
+        *ngFor="let passenger of passengers"
+        [detail]="passenger"
+      >
+      </passenger-detail>
+
       <ul>
         <li *ngFor="let passenger of passengers; let i = index">
-          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}:{{ passenger.fullname }}
-          <p>{{ passenger | json }}</p>
-          <div class="date">
-            Check in date:
-            {{
-              passenger.checkInDate
-                ? (passenger.checkInDate | date: "yMMMd" | uppercase)
-                : "Not checked In"
-            }}
-          </div>
-          <div class="children" >
-           Children: {{ passenger.children?.length || 0 }}
+          <div class="children">
+            Children: {{ passenger.children?.length || 0 }}
           </div>
         </li>
       </ul>
-  </div>
-
-  `
+    </div>
+  `,
 })
-export class PassengerDashboardComponent implements OnInit{
-  passengers: Passenger[] = [
-  ];
-  constructor(){
-
-  }
-  ngOnInit(){
-    console.log("ngOnInit")
-    this.passengers=[
+export class PassengerDashboardComponent implements OnInit {
+  passengers: Passenger[] = [];
+  constructor() {}
+  ngOnInit() {
+    console.log("ngOnInit");
+    this.passengers = [
       {
         id: 1,
         fullname: "Stephen",
         checkedIn: true,
         checkInDate: 15135125454235,
-        children:[{name:"bob",age:3},{name:"PopMonster",age:12}]
+        children: [
+          { name: "bob", age: 3 },
+          { name: "PopMonster", age: 12 },
+        ],
       },
       {
         id: 2,
         fullname: "Rose",
         checkedIn: false,
         checkInDate: null,
-        children:[{name:"bob",age:3}]
+        children: [{ name: "bob", age: 3 }],
       },
       {
         id: 3,
         fullname: "Arber",
         checkedIn: true,
         checkInDate: 15135125454235,
-        children:null
+        children: null,
       },
       {
         id: 4,
         fullname: "Renald",
         checkedIn: false,
         checkInDate: null,
-        children:null
+        children: null,
       },
       {
         id: 5,
         fullname: "Fabjola",
         checkedIn: true,
         checkInDate: 15135125454235,
-        children:null
-      },]
+        children: null,
+      },
+    ];
   }
 }
