f0734fc23851470be170259f8d535ebf1e1980a5
Arber Gjonaj <agjonaj@ikub.local>
2021-01-14 11:58:54 +0100

    Route params, data-fetching with switchMap

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -1,37 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
-import { PassangerDasboardService } from './../passenger-dashboard/passanger-dashboard.service';
+import "rxjs/add/operator/switchMap";
 
-import { Passenger } from './../../Models/passenger.interface';
+import { PassangerDasboardService } from "./../passenger-dashboard/passanger-dashboard.service";
+
+import { Passenger } from "./../../Models/passenger.interface";
 
 @Component({
-  selector: 'passenger-viewer',
-  styleUrls: ['passenger-viewer.component.scss'],
-  template:`
-  <div>
-   <passenger-form
-   [detail]="passenger"
-   (update)="onUpdatePassenger($event)">
-   </passenger-form>
-   </div>
-  `
+  selector: "passenger-viewer",
+  styleUrls: ["passenger-viewer.component.scss"],
+  template: `
+    <div>
+      <passenger-form [detail]="passenger" (update)="onUpdatePassenger($event)">
+      </passenger-form>
+    </div>
+  `,
 })
-export class PassengerViewerComponent implements OnInit{
-  passenger:Passenger;
- constructor(private passengerService:PassangerDasboardService){}
- ngOnInit(){
-   this.passengerService
-   .getPassenger(1)
-   .subscribe((data:Passenger)=>{
-    this.passenger=data
-   });
-
- }
- onUpdatePassenger(event: Passenger){
-   this.passengerService
-   .updatePassenger(event)
-   .subscribe((data:Passenger)=>{
-     this.passenger = Object.assign({},this.passenger,event)
-   })
- }
+export class PassengerViewerComponent implements OnInit {
+  passenger: Passenger;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private passengerService: PassangerDasboardService
+  ) {}
+  ngOnInit() {
+    this.route.params
+      .switchMap((data: Passenger) => {
+        return this.passengerService.getPassenger(data.id);
+      })
+      .subscribe((data: Passenger) => {
+        this.passenger = data;
+      })
+  }
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, event);
+      });
+  }
 }
