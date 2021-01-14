fbf91227c337ec6b7df062ee86b40824540f7255
Arber Gjonaj <agjonaj@ikub.local>
2021-01-14 10:50:24 +0100

    Base href and RouterModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
+import { RouterModule } from "@angular/router";
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 import { AppComponent } from "./app.component";
@@ -10,11 +10,11 @@ import { AppComponent } from "./app.component";
   declarations: [AppComponent],
   imports: [
     //angular modules
-     BrowserModule,
-     CommonModule,
-     //costum modules
-     PassengerDashboardModule
-    ],
+    BrowserModule,
+    CommonModule,
+    //costum modules
+    PassengerDashboardModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
