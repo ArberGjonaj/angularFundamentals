28479365e21b303488cf0ff84d6591d4eac391f6
Arber Gjonaj <agjonaj@ikub.local>
2021-01-14 11:11:34 +0100

    Understanding routerLink

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from "@angular/core";
 
-
-
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
+      <a [routerLink]="['/']">Home </a>
+      <a [routerLink]="['/oops']">404 </a>
       <router-outlet></router-outlet>
     </div>
   `,
 })
-export class AppComponent {
-
-}
+export class AppComponent {}
