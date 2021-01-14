1868eae6d085c4312571f0fabc9871d0817fec3f
Arber Gjonaj <agjonaj@ikub.local>
2021-01-14 10:59:29 +0100

    Root module routes and outlet

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home.component";
+
+const routes: Routes= [
+  { path:'',component: HomeComponent,pathMatch:'full' }
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,HomeComponent],
   imports: [
     //angular modules
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     //costum modules
     PassengerDashboardModule,
   ],
---
+++ app/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homee',
+  template: `
+  <div>
+    Airline Passenger App
+</div>
+`
+})
+
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
